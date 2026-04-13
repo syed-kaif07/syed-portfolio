ac0e51fd2ef9efa50e69d3cb271d56de8abdf6c7
syed-kaif07 <syedkaifuddin777@gmail.com>
2026-04-13 14:44:23 +0530

    fix:project section

--- src/components/sections/ProjectsSection.tsx
+++ src/components/sections/ProjectsSection.tsx
@@ -51,8 +51,8 @@ const projects: Project[] = [
     solution: "Built a system that generates dynamic travel itineraries using APIs",
     impact: "Generated personalized itineraries instantly based on user preferences.",
     stack: ["HTML", "CSS", "JavaScript", "Django", "APIs"],
-    github: "/404",
-    live: "/404",
+    github: "/NotFound.tsx",
+    live: "/NotFound.tsx",
     video: "/videos/travel.mp4",
   },
 ];
